ab0bafcf4d2074810f659dda0e2c2f993e14661a
Jack Wattanapong <w.thongsoi@gmail.com>
2023-12-26 21:55:58 +0700

    pointer

--- beginner/pointer/pointer.go
+++ beginner/pointer/pointer.go
@@ -5,4 +5,12 @@ import "log"
 func main() {
 	myString := "Green"
 	log.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	log.Println("After func is called, myString is set to", myString)
+}
+
+func changeUsingPointer(s *string) {
+
+	newValue := "Red"
+	*s = newValue
 }
