1eb9301838a59d7272bd4b6f0532fa9b770a3fe5
Jack Wattanapong <w.thongsoi@gmail.com>
2024-02-25 19:01:17 +0700

    slices

---
+++ beginner/slice/slicebtd.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var courseName []string
+	courseName = []string{"Java", "Python"}
+	fmt.Println(courseName)
+	courseName = append(courseName, "C", "C#", "HTML", "CSS", "JS") //add C to slice with append
+	fmt.Println(courseName)
+	courseWeb := courseName[4:7] //select only last 3 arrays
+	fmt.Println(courseWeb)
+	courseWeb = courseName[:4] //select only first 4 arrays
+	fmt.Println(courseWeb)
+}
